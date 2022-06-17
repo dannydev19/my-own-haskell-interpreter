354a79d90cde7d9edbda17f1d8ada5579864e8f9
Danilo <nunezdaani@mail.com>
2022-06-17 17:39:11 -0300

    fix while tipos

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -26,7 +26,7 @@ checkProgram(Program mainbody) =
       then checkMainBodyNombres(mainbody, [])
       else checkMainBodyTipos(mainbody, [])
 
-
+--BODY
 
 checkMainBodyNombres(Com stmt:xs, env) = checkStmtNombres(stmt, env) ++ checkMainBodyNombres(xs, env)
 checkMainBodyNombres(Decl (VarDef type_def name_def):xs, env) = 
@@ -39,6 +39,7 @@ checkMainBodyNombres([], env) = []
 checkBodyNombres(x:xs, env) = checkStmtNombres(x, env) ++ checkBodyNombres(xs, env)
 checkBodyNombres([], env) = []
 
+--STMT
 
 checkStmtNombres(StmtExpr expr, env) = checkExprNombres(expr, env)
 checkStmtNombres(If expr fst_body snd_body, env) = 
@@ -46,6 +47,7 @@ checkStmtNombres(If expr fst_body snd_body, env) =
 checkStmtNombres(While expr body, env) = checkExprNombres(expr, env) ++ checkBodyNombres(body, env)
 checkStmtNombres(PutChar expr, env) = checkExprNombres(expr, env)
 
+--EXPR
 
 checkExprNombres(Var vardef_name, env) =
    if yaDeclarada vardef_name env
@@ -109,57 +111,66 @@ getTipo(GetChar, env) = TyChar
 
 
 
---   ACA EMPIEZA LA MAGIA
-
-checkMainBodyTipos(Com stmt:xs, env) = checkStmtTipos(stmt, env) ++ checkMainBodyTipos(xs, env)
-checkMainBodyTipos(Decl (VarDef type_def name_def):xs, env) = 
-   checkMainBodyTipos(xs, env ++ [(type_def, name_def)])
-checkMainBodyTipos([], env) = []
 
 
-checkBodyTipos(x:xs, env) = checkStmtTipos(x, env) ++ checkBodyTipos(xs, env)
-checkBodyTipos([], env) = []
+-- ACA EMPIEZA LA MAGIA
+
+-- BODY
+
+checkMainBodyTipos (Com stmt:xs, env) = checkStmtTipos(stmt, env) ++ checkMainBodyTipos(xs, env)
+checkMainBodyTipos (Decl (VarDef type_def name_def):xs, env) = 
+   checkMainBodyTipos (xs, env ++ [(type_def, name_def)])
+checkMainBodyTipos ([], env) = []
 
 
-checkStmtTipos(StmtExpr expr, env) = checkExprTipos(expr, env)
-checkStmtTipos(If expr fst_body snd_body, env) = 
-   checkExprTipos(expr, env) ++ checkBodyTipos(fst_body, env) ++ checkBodyTipos(snd_body, env) 
-checkStmtTipos(While expr body, env) = checkExprTipos(expr, env) ++ checkBodyTipos(body, env)
-checkStmtTipos(PutChar expr, env) = 
+checkBodyTipos (x:xs, env) = checkStmtTipos(x, env) ++ checkBodyTipos(xs, env)
+checkBodyTipos ([], env) = []
+
+
+-- STMT
+
+checkStmtTipos (StmtExpr expr, env) = checkExprTipos(expr, env)
+checkStmtTipos (If expr fst_body snd_body, env) = 
+   checkExprTipos (expr, env) ++ checkBodyTipos(fst_body, env) ++ checkBodyTipos(snd_body, env) 
+checkStmtTipos (While expr body, env) = 
+   if getTipo(expr, env) == TyChar
+      then [Expected TyInt TyChar] ++ checkExprTipos(expr, env) ++ checkBodyTipos(body, env)
+      else checkExprTipos(expr, env) ++ checkBodyTipos(body, env)
+checkStmtTipos (PutChar expr, env) = 
    if (getTipo(expr, env) == TyInt)
       then checkExprTipos(expr, env) ++ [Expected TyChar TyInt]
       else checkExprTipos(expr, env)
 
+-- EXPR
 
 -- cuando compara tipos diferentes
-checkExprTipos(Binary Equ expr1 expr2, env) =
+checkExprTipos (Binary Equ expr1 expr2, env) =
    if (getTipo(expr1, env) == getTipo(expr2, env))
       then checkExprTipos(expr1, env) ++ checkExprTipos(expr2, env)
    else checkExprTipos(expr1, env) ++ checkExprTipos(expr2, env) ++ [Expected (getTipo(expr1, env)) (getTipo(expr2, env))]
 
-checkExprTipos(Binary Less expr1 expr2,env) =
-   if (getTipo(expr1,env) == getTipo(expr2,env))
+checkExprTipos (Binary Less expr1 expr2,env) =
+   if getTipo(expr1,env) == getTipo(expr2,env)
       then checkExprTipos(expr1,env) ++ checkExprTipos(expr2,env)
    else [Expected (getTipo(expr1,env)) (getTipo(expr2,env))]
 
 
-checkExprTipos(Binary bop expr1 expr2, env) =
+checkExprTipos (Binary bop expr1 expr2, env) =
    if and [getTipo(expr1, env) == TyChar, getTipo(expr2, env) == TyChar]
-      then 
-         checkExprTipos(expr1, env) ++ checkExprTipos(expr2, env) ++ [Expected TyInt TyChar] ++ [Expected TyInt TyChar]
+      then checkExprTipos(expr1, env) ++ checkExprTipos(expr2, env) ++ [Expected TyInt TyChar] ++ [Expected TyInt TyChar]
       else if or [getTipo(expr1, env) == TyChar, getTipo(expr2, env) == TyChar]
          then checkExprTipos(expr1, env) ++ checkExprTipos(expr2, env) ++ [Expected TyInt TyChar]
       else checkExprTipos(expr1, env) ++ checkExprTipos(expr2, env)
 
 
 -- cuando asigno mal el tipo
-checkExprTipos(Assign nameVar expr, env) =
+checkExprTipos (Assign nameVar expr, env) =
    if getTipoFromName nameVar env == getTipo(expr, env)
      then checkExprTipos(expr, env)
    else  checkExprTipos(expr, env) ++ [Expected (getTipoFromName nameVar env) (getTipo(expr, env))] 
 
 --caso recursivo y unico de unary
-checkExprTipos(Unary uop expr, env) = 
-   checkExprTipos(expr, env)
+checkExprTipos (Unary uop expr, env) = 
+   checkExprTipos (expr, env)
 
-checkExprTipos(expr, env) = []
+checkExprTipos (expr, env) = []
